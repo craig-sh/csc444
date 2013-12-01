d5fe516b880a79ce57484af6da0d8240989d7839
SilverioM <silveriomiranda@hotmail.com>
2013-11-30 23:53:15 -0500

    Test Cases all added -> failing a few, see error_log.txt

--- scc.py
+++ scc.py
@@ -11,8 +11,9 @@ import diff_match_patch
 
 sys.stderr = sys.stdout
 
-# Constant defining the number of commits before a full copy
-# of the file is saved (for efficiency)
+""" Constant defining the number of commits before a full copy
+of the file is saved (for efficiency)
+"""
 full_save_index  = 5
 
 class Command():
@@ -23,64 +24,70 @@ class Command():
 #               Helper Functions
 ###################################################################
 
-    
-    #branch - Which branch to switch to 
-    #writes the desired branch to a file called branch
+
+    """branch - Which branch to switch to
+    writes the desired branch to a file called branch
+    """
     def write_branch(self,branch):
         filename = self.params['filename']
         my_path = ".scc/"+filename+".info/"
         my_file = open(my_path + "branch",'w')
         my_file.write(branch)
         my_file.close()
-    
-    # Returns the name of the current branch for the given filename
+
+    """ Returns the name of the current branch for the given filename
+    """
     def get_current_branch(self, filename):
         fileDirPath = ".scc/"+filename+".info/"
-        
+
         branchFile = open(fileDirPath + "branch",'r')
         branchName = branchFile.read()
         branchFile.close()
-        
+
         return branchName
-    
-    # Returns the version data consisting of a list of dictionaries
-    # for the given filename and branch
+
+    """ Returns the version data consisting of a list of dictionaries
+    for the given filename and branch
+    """
     def read_version_data(self,filename,branch):
         branchPath = ".scc/" + filename + ".info/" + branch + "/"
-        
+
         versionFile = open(branchPath + "version",'r')
         data = pickle.load(versionFile)
         versionFile.close()
-        
-        # Pickle may convert the list to a dict if the list has only
-        # one item, so convert it back to a list if this happens
+
+        """ Pickle may convert the list to a dict if the list has only
+        one item, so convert it back to a list if this happens
+        """
         if (type(data) == dict):
             data = [data]
-        
+
         return data
-    
-    # Writes the version data consisting of a list of dictionaries
-    # for the given filename and branch
+
+    """ Writes the version data consisting of a list of dictionaries
+    for the given filename and branch
+    """
     def write_version_data(self,filename,branch,data):
         branchPath = ".scc/" + filename + ".info/" + branch + "/"
-        
+
         versionFile = open(branchPath + "version", 'w')
         pickle.dump(data, versionFile)
         versionFile.close()
-    
-    # Reconstructs the contents of a file of a given version
-    # by applying the patches one at a time
+
+    """ Reconstructs the contents of a file of a given version
+    by applying the patches one at a time
+    """
     def reconstruct_version_content(self,filename,branch,requestedVersion):
         versionData = self.read_version_data(filename, branch)
-        
+
         # Find the last version that contained the full content
         for versionInfo in reversed(versionData[:requestedVersion]):
             if versionInfo["isDiff"] == False:
                 lastFullVersion = int(versionInfo["version"])
                 break
-            
+
         branchPath = ".scc/" + filename + ".info/" + branch + "/"
-        
+
         # Check if the version requested already contains the full content
         if requestedVersion == lastFullVersion:
             # Fetch the full content
@@ -92,22 +99,23 @@ class Command():
             initialFile = open(branchPath + str(lastFullVersion),'r')
             contents = initialFile.read();
             initialFile.close()
-            
+
             diff = diff_match_patch.diff_match_patch();
-            
+
             # Apply the patches one by one
             for i in range(lastFullVersion + 1, requestedVersion + 1):
                 patchFile = open(branchPath + str(i),'r')
                 patch = pickle.load(patchFile)
                 patchFile.close()
-                
-                contents = diff.patch_apply(patch, contents)[0]
-        
-        return contents
-           
 
-    #branch - which branch the file should be created in
-    #Creates the file in the specified branch,and sets its version to 1
+                contents = diff.patch_apply(patch, contents)[0]
+
+        return contents
+
+
+    """branch - which branch the file should be created in
+    Creates the file in the specified branch,and sets its version to 1
+    """
     def create_file(self,branch):
         filename = self.params['filename']
         my_path = ".scc/"+filename+".info/"+branch+"/"
@@ -116,11 +124,12 @@ class Command():
         versionData = [{ "version": 1, "comment": "First commit", "time": datetime.datetime.now(),
                          "isDiff": False }]
         self.write_version_data(filename, branch, versionData)
-        
+
         #since this is the time the file is added , copy the whole file as version 1
         os.system("cp "+filename + " "+my_path+"1")
-        
-    # Returns whether the file has been added to the repository before or not
+
+    """ Returns whether the file has been added to the repository before or not
+    """
     def file_in_repository(self, filename):
         my_path = ".scc/"+filename+".info/"
         if(os.path.exists(my_path)):
@@ -128,9 +137,10 @@ class Command():
         else:
             return False
 
-    #Branch - Which branch to check for
-    #Returns True if the file has been added/branched before
-    #False otherwise
+    """Branch - Which branch to check for
+    Returns True if the file has been added/branched before
+    False otherwise
+    """
     def branch_exists(self,branch):
         filename = self.params['filename']
         my_path = ".scc/"+filename+".info/"+branch
@@ -140,17 +150,18 @@ class Command():
             return False
 
 
-    #command - the command for which to check arguments
-    #Retrurns True if the right arguments are supplied for the command
-    #False if incorrect Arguments are supplied,or extra arguments are supplied
+    """command - the command for which to check arguments
+    Retrurns True if the right arguments are supplied for the command
+    False if incorrect Arguments are supplied,or extra arguments are supplied
+    """
     def check_args(self,command):
         args = {'add': ['filename'],
                 'checkin'  : ['filename','comment'],
                 'checkout' : ['filename','version'],
-                'list'     : ['filename'],     
-                'branch'   : ['filename','branch'],    
-                'merge'    : ['filename','branch','to_branch'],    
-                'switch'   : ['filename','branch'],        
+                'list'     : ['filename'],
+                'branch'   : ['filename','branch'],
+                'merge'    : ['filename','branch','to_branch'],
+                'switch'   : ['filename','branch'],
                 }
         num_args = 0
         for  arg in args[command]:
@@ -166,14 +177,16 @@ class Command():
 ###################################################################
 
 
-    #command - which version control command to execute
-    #execute will run the command specified
+    """command - which version control command to execute
+    execute will run the command specified
+    """
     def execute(self,command):
         #getattr gets a function of this class with name == command
         getattr(self, command)()
-        
-    #adds tracking for the current file at version 1
-    #will terminate program if the file exists
+
+    """adds tracking for the current file at version 1
+    will terminate program if the file exists
+    """
     def add(self):
         #Check if we have info for the file already
         if(self.branch_exists("main")):
@@ -182,29 +195,35 @@ class Command():
         self.create_file("main")
         #set the branch of the file to main
         self.write_branch("main")
-        
         print "Added file '" + self.params['filename'] + "' at version 1"
 
-    #creates a new branch for the program
-    #will terminate program if the file exists
+    """creates a new branch for the program
+    will terminate program if the file exists
+    """
     def branch(self):
         branch =  self.params['branch'];
+        filename =  self.params['filename'];
         if(self.branch_exists(branch)):
             print "Error: branch '%s' already created" % branch
             sys.exit(1)
+        # Make sure file exists under source control
+        if not self.file_in_repository(filename):
+            print "Error: file: '%s' is not in source code control" % filename
+            return
         self.create_file(branch)
         print "Branch '%s' created" % branch
 
-    #switches the branch for the file given
-    #will terminate if the branch doesn't exist
+    """switches the branch for the file given
+    will terminate if the branch doesn't exist
+    """
     def switch(self):
         filename = self.params['filename']
         branch = self.params['branch']
-        #Check to see if the branch exists 
+        #Check to see if the branch exists
         if not self.branch_exists(branch):
             print "Error: Can't switch to non-existant branch: '%s'" % branch
             sys.exit(1)
-        #Don't do anyhting if we are already on the branch    
+        #Don't do anyhting if we are already on the branch
         if self.get_current_branch(filename) == branch :
             print "Doing nothing, already on branch '%s'" % branch
             return
@@ -212,52 +231,53 @@ class Command():
         self.write_branch(self.params['branch'])
         #checkout the most recent version of the file on branch
         versionData = self.read_version_data(filename, branch)
-        version = versionData[-1]["version"] 
-        
+        version = versionData[-1]["version"]
+
         # Get the content and write it out
         content = self.reconstruct_version_content(filename, branch, version)
-        
+
         curFile = open(filename, 'w')
         curFile.write(content)
         curFile.close()
         print "Switched to branch '%s'" % branch
-        
-    # Checks in the file to the repository
+
+    """ Checks in the file to the repository
+    """
     def checkin(self):
         filename = self.params['filename']
 
         # Make sure file exists under source control
         if not self.file_in_repository(filename):
-            print "Error: file not under source control"
+            print "Error: file: '"+filename+"' not under source control"
             return
-        
+
         branch = self.get_current_branch(filename)
         comment = self.params['comment']
-        
+
         # Get the version data for the file
         versionData = self.read_version_data(filename, branch)
-        
+
         lastVersion = versionData[-1]["version"]
         newVersion = lastVersion + 1
-        
+
         # Compute the diffs between the file in the repository and the current file
         previousContent = self.reconstruct_version_content(filename, branch, lastVersion)
-        
+
         curFile = open(filename, 'r')
         newContent = curFile.read()
         curFile.close()
-        
+
         diff = diff_match_patch.diff_match_patch()
         patch = diff.patch_make(previousContent, newContent)
-        
+
         # Make sure the files are actually different
         if not patch:
             print "No diffs found, repository already contains latest version"
             return
-        
+
         branchPath = ".scc/" + filename + ".info/" + branch + "/"
         contentsFile = open(branchPath + str(newVersion), 'w')
-        
+
         # Check whether we should save a full copy or only the diffs
         if ((newVersion - 1) % full_save_index) == 0:
             # Save a full copy
@@ -267,20 +287,23 @@ class Command():
         else:
             # Save the diffs
             pickle.dump(patch, contentsFile);
-            
+
             isDiff = True
-            
+
         contentsFile.close()
-        
+
         # Create the new version entry
         versionEntry = { "version": newVersion, "comment": comment, "time": datetime.datetime.now(),
                          "isDiff": isDiff }
         versionData.append(versionEntry)
-        
+
         self.write_version_data(filename, branch, versionData)
-        
+
         print "Checked in version " + str(newVersion) + " with comment '" + comment + "'"
-        
+
+    """ checks a specific version of a file out of the repository
+    if there is no version specified, gets the latest version
+    """
     def checkout(self):
         filename = self.params['filename']
 
@@ -288,10 +311,10 @@ class Command():
         if not self.file_in_repository(filename):
             print "Error: file not under source control"
             return
-        
+
         branch = self.get_current_branch(filename)
         version = int(self.params['version'])
-        
+
         # Get the latest version
         versionData = self.read_version_data(filename, branch)
         lastVersion = versionData[-1]["version"]
@@ -300,42 +323,48 @@ class Command():
         if version > lastVersion or version < 1:
             print "Error: version number is out of bounds"
             return
-        
+
         # Get the content and write it out
         content = self.reconstruct_version_content(filename, branch, version)
-        
+
         curFile = open(filename, 'w')
         curFile.write(content)
         curFile.close()
-        
+
         print "Checked out version " + str(version) + " of file '" + filename + \
-              " from branch '" + branch + "'"
-    
+              "' from branch '" + branch + "'"
+
+    """ Creates a file containing the suggested merge for two different branches.
+    Returns an error if no simple merge is possible
+    """
     def merge(self):
-        print "TODO merge " + " " +  self.params['filename'] + " " + self.params['branch'] +" " + self.params['to_branch'] 
-    
+        print "TODO merge " + " " +  self.params['filename'] + " " + self.params['branch'] +" " + self.params['to_branch']
+
+    """ Lists all of the versions (with comments) and time associated with a file
+    in the current branch
+    """
     def list(self):
         filename = self.params['filename']
-        
+
         # Make sure file exists under source control
         if not self.file_in_repository(filename):
             print "Error: file not under source control"
             return
-        
+
         branch = self.get_current_branch(filename)
 
         print "Listing versions for '" + filename + "' in branch '" + branch + "'\n"
-        
+
         # Get the version data and print its contents
         data = self.read_version_data(filename, branch)
         for version in data:
             print "Version " + str(version["version"])
-            print "\tComment: " + version["comment"]
-            print "\tDate: " + version["time"].strftime("%c")
+            print "    Comment: " + version["comment"]
+            #print "\tDate: " + version["time"].strftime("%c")
             print "\n"
 
 def main(argv):
-    #remove our command out of the arguments and save it 
+    #remove our command out of the arguments and save it
     command = argv.pop(0)
     #make sure we have a valid command
     if not command in ("add","branch","merge","checkin","checkout","list","switch"):
@@ -351,20 +380,20 @@ def main(argv):
     params = {}
     for opt,arg in opts:
         if opt in ("-f","--filename"):
-            params['filename'] = arg 
-        elif opt in ("-c","--comment"):    
-            params['comment'] = arg 
-        elif opt in ("-v","--version"):    
-            params['version'] = arg 
-        elif opt in ("-b","--branch"):    
-            params['branch'] = arg 
-        elif opt in ("-t","--to_branch"):    
-            params['to_branch'] = arg 
-        elif opt in ("-s","--switch"):    
-            params['to_branch'] = arg 
+            params['filename'] = arg
+        elif opt in ("-c","--comment"):
+            params['comment'] = arg
+        elif opt in ("-v","--version"):
+            params['version'] = arg
+        elif opt in ("-b","--branch"):
+            params['branch'] = arg
+        elif opt in ("-t","--to_branch"):
+            params['to_branch'] = arg
+        elif opt in ("-s","--switch"):
+            params['to_branch'] = arg
 
     #call the apporpriate command with the arguments
-    commandObj = Command(params)      
+    commandObj = Command(params)
     commandObj.check_args(command)
     commandObj.execute(command)
 
--- test_scc.py
+++ test_scc.py
@@ -2,25 +2,27 @@ import os,sys
 import sys, getopt
 from subprocess import call
 
+#To run: python test_scc.py
+
 def validate(test_num, test):
 
-    # Set the paths for the test files 
+    # Set the paths for the test files
     output_path = "Test_Actual_Results/Test%d.txt" %test_num
     expected_path = "Test_Expected_Results/Test%d.txt" %test_num
-    
+
     # Make a call to the scc program with the desired test conditions
     # saving the results to a text file in the output path
     os.system(test + " > " + output_path)
-    
+
     # Open the outputted file and expected results for comparison
     expected_results_file  = open(expected_path, 'r')
     actual_results_file = open(output_path, 'r')
-    
+
     # Read the information contained in the files
     expected_results = expected_results_file.readlines()
     actual_results = actual_results_file.readlines()
-    
-    # Print the results based on whether the results match the expected 
+
+    # Print the results based on whether the results match the expected
     if (expected_results == actual_results):
         print "Test %d: Passed" % test_num
         print "	Command Run: " + test
@@ -30,38 +32,144 @@ def validate(test_num, test):
 	print "	Command Run: " + test
         print "	Expected Results: " + str(expected_results)
         print "	Actual Results: " + str(actual_results)
-       
-    # Close the files and return 
+
+    # Close the files and return
     expected_results_file.close()
     actual_results_file.close()
 
 def main():
-    
-    # Set up files for testing in main directory 
+
+    # Set up files for testing in main directory
     os.system("cp Test_Input_Files/*.test .")
 
     test_number = 1
-    
-    """ Run unit tests on command parsing """
-     
-    # Test 1: Invalid command 
-    test_case = "python scc.py fake_command" 
+
+    """ Run tests on command parsing """
+
+    # Test1: Invalid command
+    test_case = "python scc.py fake_command"
     validate (test_number, test_case)
     test_number += 1
-        
-    # Test 2: Wrong number of arguments 
 
+    # Test2: Missing arguments
     test_case = "python scc.py checkin"
     validate (test_number, test_case)
-    test_number += 1   
-    
-    # Test 3: 
-    test_case = "python scc.py checkin -f a.test -c lol"
+    test_number += 1
+
+    """ Run tests on add """
+
+    # Test3: Add a file to be tracked
+    test_case = "python scc.py add -f a.test"
     validate (test_number, test_case)
     test_number += 1
 
-    # Remove test files from main directory 
+    # Test4: Add a file to be tracked that's been added before
+    test_case = "python scc.py add -f a.test"
+    validate (test_number, test_case)
+    test_number += 1
+
+    """ Run tests on branch """
+
+    # Test5: Branch a file that's been added
+    test_case = "python scc.py branch -f a.test -b test_branch"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test6: Recreate branch that exists
+    test_case = "python scc.py branch -f a.test -b test_branch"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test7: Branch a file that does not exist
+    test_case = "python scc.py branch -f z.test -b test_branch2"
+    validate (test_number, test_case)
+    test_number += 1
+
+    """ Run tests on switch """
+
+    # Test8: Switch to a branch that exists for a file that exists
+    test_case = "python scc.py switch -f a.test -b test_branch"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test9: Switch to branch that does not exist
+    test_case = "python scc.py switch -f a.test -b test_branch2"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test10: Switch to branch that you are already in
+    test_case = "python scc.py switch -f a.test -b test_branch"
+    validate (test_number, test_case)
+    test_number += 1
+
+    """ Run tests on checkin """
+
+    # Test11: Check in file not under source control
+    test_case = "python scc.py checkin -f b.test -c file does not exist"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test12: Check in file with no changes, i.e. latest version
+    test_case = "python scc.py checkin -f a.test -c version 2"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Write changes to file a.test before proceeding with next test
+    os.system("echo 'This is a newline' >> a.test")
+    #test = raw_input()
+
+    # Test13: Check in file with changes (this is on the new_branch)
+    test_case = "python scc.py checkin -f a.test -c version 2"
+    validate (test_number, test_case)
+    test_number += 1
+
+    """ Run tests on checkout """
+
+    # Test14: Check out file that doesn't exist
+    test_case = "python scc.py checkout -f z.test"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test15: Check out version that doesn't exist
+    test_case = "python scc.py checkout -f a.test -v 3"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test16: Check out file -> version 1
+    test_case = "python scc.py checkout -f a.test -v 1"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test17: Check out latest version
+    test_case = "python scc.py checkout -f a.test"
+    validate (test_number, test_case)
+    test_number += 1
+
+    """ Run tests on list """
+
+    # Test18: List a file that doesn't exist
+    test_case = "python scc.py list -f z.test"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # Test19: List a file in the current branch (on version 2)
+    test_case = "python scc.py list -f a.test"
+    validate (test_number, test_case)
+    test_number += 1
+
+    # switch to main branch in order to list
+    os.system("python scc.py switch -f a.test -b branch")
+
+    # Test20: List a file in the main branch (unchanged)
+    test_case = "python scc.py list -f a.test"
+    validate (test_number, test_case)
+    test_number += 1
+
+    """ Run tests on merge """
+
+    """ Remove test files from main directory """
     os.system("rm *.test")
+    os.system("rm -rf .scc")
 
 if __name__ == "__main__":
     main()
