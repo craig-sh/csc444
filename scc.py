0afcd420ac3a98eae3a12dd2eb04781f2be2052a
Edmund Phung <edmund@live.ca>
2013-12-02 01:43:31 -0500

    cleaned up code and added support for pydoc

--- scc.py
+++ scc.py
@@ -2,6 +2,16 @@
 
 #run example : ./scc.py add  --filename lol.c
 
+"""
+This file implements the source code control system as described in the
+specification and design documents. Documentation for this file can be
+generated using pydoc. This file can be run using the following
+command:
+
+    ./scc.py command  -[flag] [parameters]
+
+"""
+
 import os,sys
 import sys, getopt
 import pickle
@@ -14,9 +24,13 @@ sys.stderr = sys.stdout
 """ Constant defining the number of commits before a full copy
 of the file is saved (for efficiency)
 """
-full_save_index  = 5
+full_save_frequency  = 5
 
 class Command():
+    """
+    Class for storing parameters of a command and executing commands
+    """
+    
     def __init__(self,params):
         self.params = params
 
@@ -24,20 +38,26 @@ class Command():
 #               Helper Functions
 ###################################################################
 
-
-    """branch - Which branch to switch to
-    writes the desired branch to a file called branch
-    """
     def __write_branch(self,branch):
+        """ Makes the specified branch the active branch by writing
+        the desired branch to a file called branch
+        
+        branch -- the name of the branch to switch to
+        """
+    
         filename = self.params['filename']
         my_path = ".scc/"+filename+".info/"
         my_file = open(my_path + "branch",'w')
         my_file.write(branch)
         my_file.close()
 
-    """ Returns the name of the current branch for the given filename
-    """
+
     def __get_current_branch(self, filename):
+        """ Returns the name of the current branch for the given filename
+        
+        filename -- the file's name
+        """
+        
         fileDirPath = ".scc/"+filename+".info/"
 
         branchFile = open(fileDirPath + "branch",'r')
@@ -46,10 +66,14 @@ class Command():
 
         return branchName
 
-    """ Returns the version data consisting of a list of dictionaries
-    for the given filename and branch
-    """
     def __read_version_data(self,filename,branch):
+        """ Returns the version data consisting of a list of dictionaries
+        for the given filename and branch
+        
+        filename -- the file's name
+        branch -- the branch's name
+        """
+        
         branchPath = ".scc/" + filename + ".info/" + branch + "/"
 
         versionFile = open(branchPath + "version",'r')
@@ -64,20 +88,28 @@ class Command():
 
         return data
 
-    """ Writes the version data consisting of a list of dictionaries
-    for the given filename and branch
-    """
     def __write_version_data(self,filename,branch,data):
+        """ Writes the version data consisting of a list of dictionaries
+        for the given filename and branch
+        
+        filename -- the file's name
+        branch -- the branch's name
+        """
+        
         branchPath = ".scc/" + filename + ".info/" + branch + "/"
 
         versionFile = open(branchPath + "version", 'w')
         pickle.dump(data, versionFile)
         versionFile.close()
 
-    """ Reconstructs the contents of a file of a given version
-    by applying the patches one at a time
-    """
     def __reconstruct_version_content(self,filename,branch,requestedVersion):
+        """ Reconstructs the contents of a file of a given version
+        by applying the necessary patches
+        
+        filename -- the file's name
+        branch -- the branch's name
+        requestedVersion -- the version to reconstruct
+        """
         versionData = self.__read_version_data(filename, branch)
 
         # Find the last version that contained the full content
@@ -112,11 +144,12 @@ class Command():
 
         return contents
 
-
-    """branch - which branch the file should be created in
-    Creates the file in the specified branch,and sets its version to 1
-    """
     def __create_file(self,branch):
+        """ Creates the file in the specified branch and sets its version to 1
+        
+        branch -- which branch the file should be created in
+        """
+        
         filename = self.params['filename']
         my_path = ".scc/"+filename+".info/"+branch+"/"
         os.system("mkdir -p " +my_path)
@@ -125,23 +158,27 @@ class Command():
                          "isDiff": False }]
         self.__write_version_data(filename, branch, versionData)
 
-        #since this is the time the file is added , copy the whole file as version 1
+        # Since this is the time the file is added , copy the whole file as version 1
         os.system("cp "+filename + " "+my_path+"1")
 
-    """ Returns whether the file has been added to the repository before or not
-    """
     def __file_in_repository(self, filename):
+        """ Returns whether the file has been added to the repository before or not
+        
+        filename -- the file's name
+        """
+        
         my_path = ".scc/"+filename+".info/"
         if(os.path.exists(my_path)):
             return True
         else:
             return False
 
-    """Branch - Which branch to check for
-    Returns True if the file has been added/branched before
-    False otherwise
-    """
     def __branch_exists(self,branch):
+        """ Returns whether the file has been added/branched before
+        
+        branch -- Which branch to check for
+        """
+        
         filename = self.params['filename']
         my_path = ".scc/"+filename+".info/"+branch
         if(os.path.exists(my_path)):
@@ -149,12 +186,13 @@ class Command():
         else:
             return False
 
-
-    """command - the command for which to check arguments
-    Retrurns True if the right arguments are supplied for the command
-    False if incorrect Arguments are supplied,or extra arguments are supplied
-    """
     def check_args(self,command):
+        """ Retrurns True if the right arguments are supplied for the command and
+        False if incorrect or extra arguments are supplied
+        
+        command -- the command name for which to check arguments
+        """
+        
         args = {'add'           : ['filename'],
                 'checkin'       : ['filename','comment'],
                 'checkout'      : ['filename','version'],
@@ -166,8 +204,9 @@ class Command():
                 'switch'        : ['filename','branch'],
                 }
         num_args = 0
+        
         for  arg in args[command]:
-            #make sure the right args are specified
+            # Make sure the right args are specified
             if not arg in self.params:
                 print "Error: argument "+arg+" required"
                 sys.exit(1)
@@ -178,60 +217,74 @@ class Command():
 #               Primary Functions
 ###################################################################
 
-
-    """command - which version control command to execute
-    execute will run the command specified
-    """
     def execute(self,command):
+        """ Runs the command specified
+        
+        command -- name of version control command to execute
+        """
+        
         #getattr gets a function of this class with name == command
         getattr(self, command)()
 
-    """adds tracking for the current file at version 1
-    will terminate program if the file exists
-    """
     def add(self):
+        """ Adds tracking for the current file and sets its version to 1,
+        will terminate if the file is already under source control
+        """
+        
         #Check if we have info for the file already
         if(self.__branch_exists("main")):
             print "Error: file: "+self.params['filename'] +" already added"
             sys.exit(1)
+            
         self.__create_file("main")
-        #set the branch of the file to main
+        
+        #Set the branch of the file to main
         self.__write_branch("main")
+        
         print "Added file '" + self.params['filename'] + "' at version 1"
 
-    """creates a new branch for the program
-    will terminate program if the file exists
-    """
     def branch(self):
+        """ Creates a new branch for the specified filename,
+        will terminate program if the branch already exists
+        """
+        
         branch =  self.params['branch'];
         filename =  self.params['filename'];
+        
         if(self.__branch_exists(branch)):
             print "Error: branch '%s' already created" % branch
             sys.exit(1)
+            
         # Make sure file exists under source control
         if not self.__file_in_repository(filename):
             print "Error: file: '%s' is not in source code control" % filename
             return
+        
         self.__create_file(branch)
         print "Branch '%s' created" % branch
 
-    """switches the branch for the file given
-    will terminate if the branch doesn't exist
-    """
     def switch(self):
+        """ Switches the branch for the file given,
+        will terminate if the branch doesn't exist
+        """
+        
         filename = self.params['filename']
         branch = self.params['branch']
-        #Check to see if the branch exists
+        
+        # Check to see if the branch exists
         if not self.__branch_exists(branch):
-            print "Error: Can't switch to non-existant branch: '%s'" % branch
+            print "Error: Can't switch to non-existent branch: '%s'" % branch
             sys.exit(1)
-        #Don't do anyhting if we are already on the branch
+            
+        # Don't do anything if we are already on the branch
         if self.__get_current_branch(filename) == branch :
             print "Doing nothing, already on branch '%s'" % branch
             return
-        #update the branch file to switch the branch
+        
+        # Update the branch file to switch the branch
         self.__write_branch(self.params['branch'])
-        #checkout the most recent version of the file on branch
+        
+        # Checkout the most recent version of the file on branch
         versionData = self.__read_version_data(filename, branch)
         version = versionData[-1]["version"]
 
@@ -243,9 +296,10 @@ class Command():
         curFile.close()
         print "Switched to branch '%s'" % branch
 
-    """ Checks in the file to the repository
-    """
     def checkin(self):
+        """ Checks in the specified file to the repository
+        """
+        
         filename = self.params['filename']
 
         # Make sure file exists under source control
@@ -281,7 +335,7 @@ class Command():
         contentsFile = open(branchPath + str(newVersion), 'w')
 
         # Check whether we should save a full copy or only the diffs
-        if ((newVersion - 1) % full_save_index) == 0:
+        if ((newVersion - 1) % full_save_frequency) == 0:
             # Save a full copy
             contentsFile.write(newContent)
 
@@ -303,10 +357,11 @@ class Command():
 
         print "Checked in version " + str(newVersion) + " with comment '" + comment + "'"
 
-    """ checks a specific version of a file out of the repository
-    if there is no version specified, gets the latest version
-    """
     def checkout(self):
+        """ Checks a specific version of a file out of the repository,
+        if there is no version specified, gets the latest version
+        """
+        
         filename = self.params['filename']
 
         # Make sure file exists under source control
@@ -342,15 +397,16 @@ class Command():
         print "Checked out version " + str(version) + " of file '" + filename + \
               "' from branch '" + branch + "'"
 
-    """ Creates a file containing the suggested merge for two different branches.
-    Returns an error if no simple merge is possible
-    """
     def merge(self):
+        """ Creates a file containing the suggested merge for two different branches.
+        Returns an error if no simple merge is possible
+        """
+        
         filename = self.params['filename']
         from_branch = self.params['branch']
         to_branch = self.params['to_branch']
 
-        #make sure both branches exist
+        # Make sure both branches exist
         if not self.__branch_exists(from_branch):
             print "Error: Branch '%s' does not exist" % from_branch
             return
@@ -358,37 +414,37 @@ class Command():
             print "Error: Branch '%s' does not exist" % to_branch
             return
 
-        #get the previous and current version file in the source branch
+        # Get the previous and current version file in the source branch
         version_data = self.__read_version_data(filename, from_branch)
         curr_version = version_data[-1]["version"]
         prev_version = curr_version - 1
 
-        #Make sure the branch has an update
+        # Make sure the branch has an update
         if prev_version < 1:
             print "Error: Cannot suggest merge on a newly branched file"
             return
 
-        #diff the two versions to isolate the last change in a patch
+        # Diff the two versions to isolate the last change in a patch
         prev_content = self.__reconstruct_version_content(filename, from_branch, prev_version)
         curr_content = self.__reconstruct_version_content(filename, from_branch, curr_version)
 
         diff =  diff_match_patch.diff_match_patch()
         patch = diff.patch_make(prev_content, curr_content)
 
-        #get the contents of the file in the to_branch
+        # Get the contents of the file in the to_branch
         version_data = self.__read_version_data(filename, to_branch)
         curr_version = version_data[-1]["version"]
         content = self.__reconstruct_version_content(filename, to_branch, curr_version)
 
-        #patch the file in the latest to branch,and store if it suceeded
+        # Patch the file in the latest to branch,and store if it succeeded
         content,sucessful= diff.patch_apply(patch, content)
 
-        #Tell the user we couldn't patch if the merge failed
+        # Tell the user we couldn't patch if the merge failed
         if not sucessful[0]:
             print "Error: Unable to merge branches, branch files too different"
             return
 
-        #write out the suggested file as filename.suggested
+        # Write out the suggested file as filename.suggested
         suggested_file = open(filename + ".suggested",'w')
         suggested_file.write(content)
         suggested_file.close()
@@ -396,10 +452,11 @@ class Command():
         print "Merged branch %s to best resemble update in branch %s" %( to_branch, from_branch)
         print "Wrote out suggested merge in %s.suggested" % filename
 
-    """ Lists all of the versions (with comments) and time associated with a file
-    in the current branch
-    """
     def list(self):
+        """ Lists all of the versions (with comments) and time associated with a file
+        in the current branch
+        """
+        
         filename = self.params['filename']
 
         # Make sure file exists under source control
@@ -418,11 +475,12 @@ class Command():
             print "    Comment: " + version["comment"]
             #print "\tDate: " + version["time"].strftime("%c")
 
-    """ Lists all of the different branches that exist for a file
-    Branch names are the same as the folder names in  the directory
-    containing the metadata for a file
-    """
     def list_branches(self):
+        """ Lists all of the different branches that exist for a file
+        Branch names are the same as the folder names in the directory
+        containing the metadata for a file
+        """
+    
         filename = self.params['filename']
 
         # Make sure file exists under source control
@@ -434,15 +492,17 @@ class Command():
         path = ".scc/" + filename + ".info/"
 
         # Print out all of the directory names, i.e. branches
-        #os.system("ls %s -F | grep /") % path
-        #for filename in os.listdir(path):
         print os.walk(path).next()[1]
 
-
 def main(argv):
-    #remove our command out of the arguments and save it
+    """ Main function of program
+    Parses the command line arguments and executes the appropriate command
+    """
+    
+    # Remove our command out of the arguments and save it
     command = argv.pop(0)
-    #make sure we have a valid command
+    
+    # Make sure we have a valid command
     if not command in ("add","branch","merge","checkin", \
             "checkout","list","switch", "list_branches"):
         sys.exit("Invalid command " + command)
@@ -453,7 +513,8 @@ def main(argv):
     except getopt.GetoptError:
         print 'Input Error'
         sys.exit(1)
-    #fill the parameters
+        
+    # Fill the parameters
     params = {}
     for opt,arg in opts:
         if opt in ("-f","--filename"):
@@ -469,7 +530,7 @@ def main(argv):
         elif opt in ("-s","--switch"):
             params['to_branch'] = arg
 
-    #call the apporpriate command with the arguments
+    # Call the appropriate command with the arguments
     commandObj = Command(params)
     commandObj.check_args(command)
     commandObj.execute(command)
