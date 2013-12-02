12aef3bf66a2ad2205fd43374dd3ffe21c8b8db7
craig-sh <craig.henriques@mail.utoronto.com>
2013-12-01 23:21:37 -0500

    Added error checking for merge, Merge will also fail if it cannot merge

--- scc.py
+++ scc.py
@@ -349,10 +349,23 @@ class Command():
         from_branch = self.params['branch']
         to_branch = self.params['to_branch']
         
+        #make sure both branches exist
+        if not self.__branch_exists(from_branch):
+            print "Branch '%s' does not exist" % from_branch 
+            return
+        if not self.__branch_exists(to_branch):
+            print "Branch '%s' does not exist" % to_branch 
+            return
+
         #get the previous and current version file in the source branch
         version_data = self.__read_version_data(filename, from_branch)
         curr_version = version_data[-1]["version"]
         prev_version = curr_version - 1
+       
+        #Make sure the branch has an update
+        if prev_version < 1:
+            print "Cannot suggest merge on a newly branched file"
+            return
 
         #diff the two versions to isolate the last change in a patch
         prev_content = self.__reconstruct_version_content(filename, from_branch, prev_version)
@@ -366,14 +379,20 @@ class Command():
         curr_version = version_data[-1]["version"]
         content = self.__reconstruct_version_content(filename, to_branch, curr_version)
 
-        #patch the file in the latest to branch
-        content = diff.patch_apply(patch, content)[0]
+        #patch the file in the latest to branch,and store if it suceeded
+        content,sucessful= diff.patch_apply(patch, content)
+        
+        #Tell the user we couldn't patch if the merge failed
+        if not sucessful[0]:
+            print "Unable to merge branches."
+            return
+
         #write out the suggested file as filename.suggested
         suggested_file = open(filename + ".suggested",'w')
         suggested_file.write(content)
         suggested_file.close()
         
-        print "Merged branch %s to best resemble update in branch %s." %( from_branch, to_branch)
+        print "Merged branch %s to best resemble update in branch %s." %( to_branch, from_branch)
         print "Wrote out suggested merge in %s.suggested" % filename
 
     """ Lists all of the versions (with comments) and time associated with a file
